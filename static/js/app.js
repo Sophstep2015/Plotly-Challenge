c15ff70f6634acf186f12ab72851b9c4ccb37992
Stephen Lane <sophstep2015@gmail.com>
2022-01-02 23:04:56 -0600

    code commented

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,11 @@
+//Function/loop for metadata display for Demographic
 function demographic(select){
     d3.json("samples.json").then((data) => {
        let mData = data.metadata;
        console.log(mData)
        let demo = mData.filter(selectObj => selectObj.id == select);
        let demoData = demo[0];
+       //listener
        d3.select("#sample-metadata").html(""); 
        Object.entries(demoData).forEach(([key, value]) =>{
             d3.select("#sample-metadata")
@@ -11,16 +13,18 @@ function demographic(select){
        });
    });
 }
-
+//Function/loop for Barchart
 function barchart(drop)
 {
     d3.json("samples.json").then((bdata) => {
         let chart = bdata.samples;
         let barchart = chart.filter(dropObj => dropObj.id == drop);
         let barchart1 = barchart[0];
+        //set x and y variables from data for top 10 and descending 
         let y = barchart1.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse();
         let hovertext = barchart1.otu_labels;
         let x = barchart1.sample_values.slice(0, 10).reverse();
+        //Hovertext or Tooltip top ten
         let tooltip = hovertext.slice(0, 10).reverse();
         let horizontalbar = {
             y: y,
@@ -32,6 +36,7 @@ function barchart(drop)
         Plotly.newPlot("bar", [horizontalbar]);
     });
 }
+// Bubble Chart function and loop
 function bubbles(drop2)
 {
     d3.json("samples.json").then((bdata2) => {
@@ -55,7 +60,7 @@ function bubbles(drop2)
         Plotly.newPlot('bubble', [bubbleChart], layout);
     });
 }
-
+//function loop for initial load
 function initial_load()
 {
     let drop = d3.select("#selDataset");
@@ -74,7 +79,7 @@ function initial_load()
         console.log(display);
     });
 }
-
+//update page when drop down changes 
 function optionChanged(change)
 {
     demographic(change);
